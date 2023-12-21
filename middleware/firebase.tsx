64660b09e1f8fcaaf10b6ff8ebcad6d2b169ba59
Charchit <charchitbhatnagar@gmail.com>
2023-12-22 02:09:09 +0530

    fixed errors 3

--- middleware/firebase.tsx
+++ middleware/firebase.tsx
@@ -5,14 +5,20 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 
-const apiKey = process.env.NEXT_PUBLIC_FIREBASE_apiKey;
-const authDomain = process.env.NEXT_PUBLIC_FIREBASE_authDomain;
-const projectId = process.env.NEXT_PUBLIC_FIREBASE_projectId;
-const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_storageBucket;
-const messagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_measurementId;
-const appId = process.env.NEXT_PUBLIC_FIREBASE_appId;
-const measurementId = process.env.NEXT_PUBLIC_FIREBASE_measurementId;
-
+// const apiKey = process.env.NEXT_PUBLIC_FIREBASE_apiKey;
+// const authDomain = process.env.NEXT_PUBLIC_FIREBASE_authDomain;
+// const projectId = process.env.NEXT_PUBLIC_FIREBASE_projectId;
+// const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_storageBucket;
+// const messagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_measurementId;
+// const appId = process.env.NEXT_PUBLIC_FIREBASE_appId;
+// const measurementId = process.env.NEXT_PUBLIC_FIREBASE_measurementId;
+const apiKey = "AIzaSyCx_h9HmjAllyRDppdTi9RUz-JutidPjls";
+const authDomain = "todo-5e701.firebaseapp.com";
+const projectId = "todo-5e701";
+const storageBucket = "todo-5e701.appspot.com";
+const messagingSenderId = "488320199317";
+const appId = "1:488320199317:web:cdfbe6aa1114142551499c";
+const measurementId = "G-L4JMV347GR";
 const firebaseConfig = {
   apiKey: apiKey,
   authDomain: authDomain,
@@ -20,7 +26,7 @@ const firebaseConfig = {
   storageBucket: storageBucket,
   messagingSenderId: messagingSenderId,
   appId: appId,
-  measurementId: measurementId
+  measurementId: measurementId,
 };
 
 // Initialize Firebase
